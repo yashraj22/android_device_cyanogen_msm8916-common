8be64e7b28d1c5ba3df962ed29ed02531ad15a6a
Yashraj Yadav <yyashraj05@gmail.com>
2020-05-06 03:26:48 +0000

    libshims: Upgrade library dependencies for O
    Change-Id: Ib1b0817a14df3fe06035cb3196a5bcd836628693

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,13 +141,15 @@ include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += \
     $(PLATFORM_PATH)/sepolicy
 
-# Shims
+# Shim
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib64/libflp.so|libshims_flp.so \
-    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
-    /system/vendor/lib/libflp.so|libshims_flp.so \
-    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
-    /system/vendor/lib/libmmcamera2_stats_modules.so |libshim_sensormanager.so
+    /vendor/bin/mm-qcamera-daemon|libshim_camera.so \
+    /vendor/lib/libflp.so|libshims_flp.so \
+    /vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
+    /vendor/lib/libmmcamera2_stats_modules.so|libshim_camera.so \
+    /vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
+    /vendor/lib64/libflp.so|libshims_flp.so \
+    /vendor/lib64/libizat_core.so|libshims_get_process_name.so
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
